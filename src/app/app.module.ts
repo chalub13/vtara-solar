f34bb4209c30269bdab4a19b2667334cc575563d
Marco Chalub <marco.chalub@gmail.com>
2019-07-26 11:19:58 +1000

    Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,12 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faAnchor } from '@fortawesome/free-solid-svg-icons';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
-
 @NgModule({
   declarations: [AppComponent, NavbarComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  contrauctor() {
-    library.add(fas, far, fab);
-    library.add(faAnchor);
-    library.add(faPhone);
-    library.add(faTwitter);
-  }
+  contrauctor() {}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,7 @@ export class HomeComponent implements OnInit {
   mainCaption = 'VTara Solar';
   phraseCaption = '" Energy Through Renewables "';
   buttonCaptionText = 'Contact us';
-  whatWeDoHead = 'What We Do';
+  whatWeDoHead = 'What We Do?';
   whatWeDoParagraph = 'TBD';
 
   constructor() {}
