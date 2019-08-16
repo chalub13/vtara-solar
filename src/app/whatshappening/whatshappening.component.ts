3039a141e3d8807484165f3f04bcf1eab34288de
Marco Chalub <marco.chalub@gmail.com>
2019-08-16 12:15:05 +1000

    Whats Happening

---
+++ src/app/whatshappening/whatshappening.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'whatshappening',
+  templateUrl: './whatshappening.component.html',
+  styleUrls: ['./whatshappening.component.sass']
+})
+export class WhatshappeningComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
