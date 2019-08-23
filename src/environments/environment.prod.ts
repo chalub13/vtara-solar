095c791471684352cc6417bbea55a87196d47b13
Marco Chalub <marco.chalub@gmail.com>
2019-08-23 11:17:55 +1000

    Environment contentful variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  contentful: {
+    spaceId: 'b23k6aaz50f3',
+    token: 'uY2smFne-fUxyH4j7Icco3wq9XlJViXnYlGqL-isTq4'
+  }
 };
